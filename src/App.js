495e86ffe9ad5e97baf83d7d6455cdfb189e1c08
Corrina Chow <corrina.yy.chow@gmail.com>
2020-04-21 23:32:31 -0400

    test - Add rendering test

--- src/App.js
+++ src/App.js
@@ -3,78 +3,80 @@ import "./App.css";
 import { useControlledForm } from "./useControlledForm";
 
 function App() {
-	const [formValues, parsedFormData, handleInput, errors] = useControlledForm([
-		{
-			name: "firstName",
-			value: "Francis",
-			placeholder: "First Name",
-			required: true,
-		},
-		{
-			name: "lastName",
-			placeholder: "Last Name",
-		},
-		{
-			name: "email",
-			placeholder: "Email Address",
-			required: true,
-			type: "email",
-		},
-	]);
-	const inputFields = formValues.map((formInput) => {
-		const { name, placeholder, type, required, value } = formInput;
+  const [formValues, parsedFormData, handleInput, errors] = useControlledForm([
+    {
+      name: "firstName",
+      value: "Francis",
+      placeholder: "First Name",
+      required: true,
+    },
+    {
+      name: "lastName",
+      placeholder: "Last Name",
+    },
+    {
+      name: "email",
+      placeholder: "Email Address",
+      required: true,
+      type: "email",
+    },
+  ]);
+  const inputFields = formValues.map((formInput) => {
+    const { name, placeholder, type, required, value } = formInput;
 
-		return (
-			<input
-				{...{
-					key: name,
-					name,
-					value: value,
-					placeholder,
-					onChange: handleInput,
-					type,
-					required,
-				}}
-			/>
-		);
-	});
+    return (
+      <input
+        {...{
+          key: name,
+          name,
+          value: value,
+          placeholder,
+          onChange: handleInput,
+          type,
+          required,
+        }}
+      />
+    );
+  });
 
-	return (
-		<div className="App">
-			<h1>Controlled Form custom hook, example inputs :</h1>
-			<form>{inputFields}</form>
-			<section>
-				<h2>Errors Array</h2>
-				{errors.map((error) => (
-					<p>{error}</p>
-				))}
-			</section>
-			<section>
-				<h2>Output of formValues</h2>
-				<pre
-					style={{
-						textAlign: "left",
-						margin: "0 auto",
-						display: "inline-block",
-					}}
-				>
-					{JSON.stringify(formValues, undefined, 2)}
-				</pre>
-			</section>
-			<section>
-				<h2>Output of parsedFormData</h2>
-				<pre
-					style={{
-						textAlign: "left",
-						margin: "0 auto",
-						display: "inline-block",
-					}}
-				>
-					{JSON.stringify(parsedFormData, undefined, 2)}
-				</pre>
-			</section>
-		</div>
-	);
+  return (
+    <div className="App">
+      <h1>Controlled Form custom hook, example inputs :</h1>
+      <form>{inputFields}</form>
+      <section>
+        <h2>Errors Array</h2>
+        {errors.map((error, i) => (
+          <p
+            key={`${error.name}-${i}`}
+          >{`name: ${error.name}, placeholder: ${error.placeholder}`}</p>
+        ))}
+      </section>
+      <section>
+        <h2>Output of formValues</h2>
+        <pre
+          style={{
+            textAlign: "left",
+            margin: "0 auto",
+            display: "inline-block",
+          }}
+        >
+          {JSON.stringify(formValues, undefined, 2)}
+        </pre>
+      </section>
+      <section>
+        <h2>Output of parsedFormData</h2>
+        <pre
+          style={{
+            textAlign: "left",
+            margin: "0 auto",
+            display: "inline-block",
+          }}
+        >
+          {JSON.stringify(parsedFormData, undefined, 2)}
+        </pre>
+      </section>
+    </div>
+  );
 }
 
 export default App;
