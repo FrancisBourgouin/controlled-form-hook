359036e26a0d5b3da16fd1b83b6c193a9fe661fd
corrina <corrina.yy.chow@gmail.com>
2020-04-22 09:13:04 -0400

    Feat - Review controlled hook (#1)
    * feat - Add react-testing-library hooks
    
    * test - Add useControlledForm tests
    
    * test - Add rendering test
    
    * Remove debugger

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders title", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Controlled Form/i);
   expect(linkElement).toBeInTheDocument();
 });
